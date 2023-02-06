5660993b0d9d3eb870cabf08efcdd2c4b55236f3
voanhq <voanhq@gmail.com>
2023-02-07 00:17:44 +0700

    added meeting notes

---
+++ _build/jupyter_execute/Meetings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Meeting Notes 
+
+# Meeting 6th Feb 2023
+# - Need to learn Pytorch and successfully run DARTS/iDARTS code before commiting on thesis topic.
+# - Thesis Ideas:
+#   1) Different Hessian approximation methods for iDARTS
+#   2) Relationship between Hessian w.r.t $w$ & Hessian w.r.t $\alpha$
+
+# In[ ]:
+
+
+
+
